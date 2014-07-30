ce66aa212b7daa2130d2e6f713b644877e73764e
David Morton <davidlmorton@gmail.com>
2014-07-30 17:44:20 +0000

    Raise exception instead of returning None on unknown alg

--- jot/exceptions.py
+++ jot/exceptions.py
@@ -7,3 +7,11 @@ class InvalidSignature(Exception): pass
 class InvalidUUIDType(ValueError): pass
 class NoIssuerSpecified(ValueError): pass
 class TokenExpired(Exception): pass
+
+class UnrecognizedAlgorithm(RuntimeError):
+    def __init__(self, algorithm_name, *args, **kwargs):
+        super(UnrecognizedAlgorithm, self).__init__(
+            "The algorithm named (%s) is not known to the jot library" %
+            algorithm_name, *args, **kwargs)
+class UnrecognizedAlg(UnrecognizedAlgorithm): pass
+class UnrecognizedEnc(UnrecognizedAlgorithm): pass
